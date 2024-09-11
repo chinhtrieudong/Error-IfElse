909a761491700c79e7f74266d14af49049dcba4a
Nguyen Tuan <tuanmeme666@gmail.com>
2024-09-11 20:04:17 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Vui lòng nhập tuổi của bạn: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Bạn đủ tuổi trưởng thành.");
+        } else {
+            System.out.println("Bạn chưa đủ tuổi trưởng thành.");
+        }
+
+        scanner.close();
+    }
+}
